237636964454f777cb61438ac3d34d93d57d8088
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-05-05 01:55:54 +0700

    bertujuan untuk menambahkan node baru ke dalam linked list di posisi tengah atau di akhir linked list

--- double_circular_linked_list/double_circular_linked_list.cpp
+++ double_circular_linked_list/double_circular_linked_list.cpp
@@ -34,3 +34,13 @@ void addNode() {
         START = newNode; // Step 6: make the new node the first node
 
     }
+    else {
+        // Insert the new node in the middle or at the end
+        Node* current = START; // Step 1.a: start from the first node
+        Node* previous = NULL; // Step 1.b: previous node is Null Initiality
+
+        while (current != NULL && current->noMhs < newNode->noMhs) { // Step 1.c: traverse
+            previous = current; // Step 1.d: move the previous to the current node
+            current = current->next; // Step 1.e: move the current to the next node
+        }
+    }
