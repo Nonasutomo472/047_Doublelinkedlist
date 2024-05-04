ebf2ad17201afd2ac2062471acb8aa97a219daba
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-05-05 01:54:44 +0700

    proses penambahan node baru ke dalam linked list

--- double_circular_linked_list/double_circular_linked_list.cpp
+++ double_circular_linked_list/double_circular_linked_list.cpp
@@ -30,4 +30,7 @@ void addNode() {
             START->prev = newNode; // Step 4: make the first node point to the new node
 
         }
+        newNode->prev = NULL; // Step 5: make the new node point to NULL
+        START = newNode; // Step 6: make the new node the first node
+
     }
