1a30b28a7bff79eedfd81ee953dcf507debd6a2e
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-05-05 02:08:47 +0700

    bertujuan untuk menampilkan semua node dalam linked list secara terbalik, yaitu dari node terakhir hingga node pertama

--- double_circular_linked_list/double_circular_linked_list.cpp
+++ double_circular_linked_list/double_circular_linked_list.cpp
@@ -144,3 +144,13 @@ void traverse()
         }
     }
 }
+void revtraverse()
+{
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecords in descending order of roll number are:" << endl;
+        Node* currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
