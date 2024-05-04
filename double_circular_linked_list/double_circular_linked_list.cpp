ab668b1be1a7b67a9eb48babcb603ac6f733ab42
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-05-05 01:49:33 +0700

    implementasi dari sebuah program yang menggunakan linked list untuk menyimpan data mahasiswa

--- double_circular_linked_list/double_circular_linked_list.cpp
+++ double_circular_linked_list/double_circular_linked_list.cpp
@@ -9,3 +9,12 @@ struct Node
 	Node* next;
 	Node* prev;
 };
+Node* START = NULL;
+
+void addNode() {
+    Node* newNode = new Node(); // Step 1: create a new node
+    cout << "\nEnter the roll number of the student:";
+    cin >> newNode->noMhs; // assign value to the rollNumber field of the new node
+    cout << "\nEnter the name of the student:";
+    cin >> newNode->name; // assign value to the name field of the new node
+
