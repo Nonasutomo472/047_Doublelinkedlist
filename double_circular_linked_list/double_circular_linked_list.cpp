8fb82f74bfe536a31fca051b5040a201ac32db28
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-05-05 02:06:48 +0700

    bagian dari proses penghapusan node dari linked list dan fungsi bantuan untuk memeriksa apakah linked list kosong

--- double_circular_linked_list/double_circular_linked_list.cpp
+++ double_circular_linked_list/double_circular_linked_list.cpp
@@ -117,3 +117,14 @@ void deleteNode()
 
         }
     }
+    // Release the memory of the node marked as current
+    delete current;
+    cout << "\x1b[32mRecord with roll number " << rollNo << "deleted\x1b[0m" << endl;
+
+}
+
+bool listEmpty()
+{
+    return (START == NULL);
+}
+
